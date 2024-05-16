09d53d3cfc47d4e09e703ba7cef09f647dc4a77c
kimmchii <thanachotkim@hotmail.co.th>
2024-05-16 17:10:22 +0700

    added dataset modules

---
+++ modules/datasets/speaker_count_dataset.py
@@ -0,0 +1,67 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch.nn.functional import pad
+import pytorch_lightning as pl
+import librosa
+import json
+
+
+class SpeakerCountDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        data_path = self.data[idx]["path"]
+        label = int(self.data[idx]["num_speaker"])
+        audio_array, _ = librosa.load(data_path, sr=8000)
+        audio_tensor = torch.from_numpy(audio_array)
+        label_tensor = torch.tensor(label)
+        return {"audio": audio_tensor, "label": label_tensor}
+    
+class LightningSpeakerCounterDataModule(pl.LightningDataModule):
+    def __init__(self, train_data_path: str, val_data_path: str, batch_size: str=1):
+        super().__init__()
+        self.batch_size = batch_size
+
+        with open(train_data_path, "r") as f:
+            self.train_data = json.load(f)
+
+        with open(val_data_path, "r") as f:
+            self.val_data = json.load(f)
+
+    def custom_collate_fn(self, batch):
+        """
+        Customs collate function to pad the audio to the maximum length.
+        Inputs:
+            batch: List of dictionaries containing the merged audio and the target audios.
+        
+        Returns:
+            merged_audios:
+                Tensors of merged audios padded to the maximum length.
+
+            targets:
+                Tensors of target audios padded to the maximum length.
+        """
+        audios = [item["audio"] for item in batch]
+        max_len = max([len(audio) for audio in audios])
+        pad_audios = [pad(audio, (0, max_len - len(audio))).unsqueeze(0) for audio in audios]
+
+        labels = [item["label"] for item in batch]
+        audio_tensors = torch.stack(pad_audios, dim=0)
+        label_tensors = torch.stack(labels)
+        return audio_tensors, label_tensors
+
+    def setup(self, stage=None):
+        if stage == "fit" or stage is None:
+            self.train = SpeakerCountDataset(self.train_data)
+        if stage == "test" or stage is None:
+            self.test = SpeakerCountDataset(self.val_data)
+
+    def train_dataloader(self):
+        return DataLoader(self.train, batch_size=self.batch_size, shuffle=True, collate_fn=self.custom_collate_fn)
+
+    def val_dataloader(self):
+        return DataLoader(self.test, batch_size=self.batch_size, collate_fn=self.custom_collate_fn)
